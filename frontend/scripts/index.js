2a6723f84b898feda562c5d23673c8caf2ace04e
Priyanshii23 <agrawalpriyanshi123@gmail.com>
2024-12-28 14:47:20 +0530

    Log In as User Done

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,3 +1,5 @@
+import { baseUrl } from "../../backend/baseUrl.js";
+
 let form = document.getElementById("form");
 form.addEventListener("submit", function () {
     // alert("Login Successful")
@@ -7,15 +9,31 @@ form.addEventListener("submit", function () {
 
     //Email: admin@empher.com
     // Password: empher@123
-    if (email == "admin@empher.com") {
-        if (password == "empher@123empher@123") {
-            alert("Logged in as Admin")
-            window.location.href = "admin.html"
-            
+
+    
+    // Email: user@empher.com
+    // Password: user@123
+
+    if (email == "admin@empher.com" || "user@empher.com") {
+        
+        
+        if (password == "empher@123"|| "user@empher.com") {
+           
+            if(email=="admin@empher.com"){
+                alert("Logged In as Admin");
+                window.location.href = "admin.html"
+            } else {
+                alert("Login Successful");
+                window.location.href = "books.html"
+            }
+            localStorage.setItem("loginData", JSON.stringify)
+
         } else {
             alert("Please enter correct password")
         }
     } else {
         alert(" Login Failed.. Please enter correct password...")
     }
+
+
 })
