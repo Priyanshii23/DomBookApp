0f0e0deba735e60497ceb60f52008ad61331a4fd
Priyanshii23 <agrawalpriyanshi123@gmail.com>
2024-12-28 15:44:01 +0530

    Admin Page

--- backend/baseurl.js
+++ backend/baseurl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "https://short-bustling-toaster.glitch.me/"
+export const baseUrl = "https://short-bustling-toaster.glitch.me"
+
--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -4,26 +4,59 @@ import { baseUrl } from "../../backend/baseUrl.js";
 // console.log(loginData)
 
 let admin_form = document.getElementById("admin")
-admin_form.addEventListener("submit", function(){
+admin_form.addEventListener("submit", async function () {
     event.preventDefault()
     let title = admin_form.title.vallue;
     let author = admin_form.author.value;
     let category = admin_form.category.value;
 
-    let books= {title, author, category}
+    let books = { title, author, category }
 
-    fetch(`${baseUrl}/books`)
-    .then((res)=>res.json())
-    .then((data)=>{
-          method: "POST";
-          Headers:{"content-type"; "application/json" }
-         body : JSON.stringify(books)        
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-    alert("Book Added Successfully")
+
+    try {
+        await fetch( `${baseUrl}/books`,{
+         method: "POST",
+         headers:{ "content-type":"application/json"},
+         body: JSON.stringify("books")
+        });
+        alert("Books Added Successfully")
+    }
+    catch(err){
+        alert("Something went wrong")
+    }
+
+
+
+    // fetch(`${baseUrl}/books`)
+    // .then((res)=>res.json())
+    // .then((data)=>{
+    //       method: "POST";
+    //       Headers:{"content-type"; "application/json" }
+    //      body : JSON.stringify(books) 
+    //      alert("Book Added Successfully")       
+    // })
+    // .catch((err)=>{
+    //     console.log(err)
+    // })
+   
    
 })
+
+function getData() {
+    fetch(`${baseUrl}/books`)
+        .then((res) => res.josn)
+        .then((data) => {
+            console.log(data)
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+function displayData() {
+    
+
+}
 
 
